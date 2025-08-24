514e3d3c0310f85392e009c7b2001ecf8b82477b
Kritim Prasad Kafle <kritimprasadkafle@192.168.1.5>
2025-08-24 14:54:14 +0545

    Token Minting on Solana

---
+++ 5-Solana-Account-Creation/airdrop.js
@@ -0,0 +1,13 @@
+const {Connection, LAMPORTS_PER_SOL, clusterApiUrl, PublicKey} = require('@solana/web3.js');
+
+const connection = new Connection(clusterApiUrl('devnet'));
+
+async function airdrop(publicKey, amount){
+    const airdropSignature = await connection.requestAirdrop(new PublicKey(publicKey), amount);
+    await connection.confirmTransaction({signature: airdropSignature})
+
+}
+
+airdrop('naCm2gYW7K3cY1CqvjGk3Rgv7hoJhwaD6AyeHMRUHfL', LAMPORTS_PER_SOL).then(signature => {
+    console.log('Airdrop signature:', signature);    
+})
